542cdc0f2a0b492768809bf72e178b95522b482d
Oscar <o.velillaa@gmail.com>
2024-12-06 02:21:04 +0100

    updates

--- src/app/(routes)/contacto/contact.actions.ts
+++ src/app/(routes)/contacto/contact.actions.ts
@@ -8,13 +8,12 @@ import { FormDataType } from "./contact.types";
 async function send(formData: FormDataType) {
   const resend = new Resend(process.env.RESEND_API_KEY);
   try {
-    const { data } = await resend.emails.send({
+    await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["delivered@resend.dev"],
       subject: "Contacto desde el sitio web",
       html: `${formData.message}`,
     });
-    // console.log(data);
     return true;
   } catch (error) {
     console.error(error);
